1d5362f56fe832b94d1d5cb91cda0b3f535fa4b8
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-02-19 13:01:12 +0530

    Add files via upload

---
+++ Increment and Decrement/Main.java
@@ -0,0 +1,99 @@
+/*
+
+// INCREMENTATION & DECREMENTATION
+
+//PRE INCREMENTATION
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        ++a;
+        System.out.println(a);
+    }
+}
+
+// POST INCREMENTATION
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        a++;
+        System.out.println(a);
+    }
+}
+
+//PRE DECREMENTATION
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        --a;
+        System.out.println(a);
+    }
+}
+
+//POST DECREMENTATION
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        a--;
+        System.out.println(a);
+    }
+}
+
+
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = a++;
+        System.out.println(a); //7
+        System.out.println(b); //6
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = ++a;
+        System.out.println(a); //7
+        System.out.println(b); //7
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = a-- - a++ + --a + a++ + ++a ;
+        System.out.println(a); //7
+        System.out.println(b); //6
+    }
+}
+
+
+class Main{
+    public static void main(String[] agrs){
+        int a = 173;
+        int b = 34;
+        int c = 56;
+        int d = a++ + b++ + ++c + c-- + a-- + b-- + ++a + b + c--;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+
+*/
+
+
+class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        --a;
+        System.out.println(a);
+    }
+}
