313e9e8c49306b69f0e74898b095b40ee968fd42
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-02-20 11:53:14 +0530

    Refactor increment and decrement examples in Main.java

--- Increment and Decrement/Main.java
+++ Increment and Decrement/Main.java
@@ -86,7 +86,37 @@ class Main{
     }
 }
 
+
+class Main {
+    public static void main(String[] args) {
+        int m = 10;
+        int n = 10;
+        System.out.println(m++ / (++n * n--) / --m); // 0
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        int x = 001;  // octal → decimal 1
+        int y = 010;  // octal → decimal 8
+        int z = 100;  // decimal 100
+        int i = -- x + y++ - z-- - --z + ++y - --x + y-- - --x;
+        System.out.println(i); // -167
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        char C='A',c='a';  //ASCII value A=65 c=97
+        System.out.println(C++ * ++c); //65 * 97 = 6370
+    }
+}
+
+
 */
+
 
 
 
