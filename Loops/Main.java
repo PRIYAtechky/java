da46f39ce6283866ef478910c2d3331b1640c015
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-03-19 19:41:10 +0530

    Create Main class with Hello World output and loop comments
    Added a Main class with a main method that prints 'Hello World!' five times and includes comments on loop types.

---
+++ Loops/Main.java
@@ -0,0 +1,19 @@
+/* 
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
+
+//LOOPS
+1) for loop
+2) while loop
+3) do-while loop
+
+
+*/
