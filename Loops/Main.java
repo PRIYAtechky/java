729022c7a59efda1ee6509226138f2a3dd81dc7a
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-03-21 11:20:35 +0530

    Refactor Main.java to include loop examples

--- Loops/Main.java
+++ Loops/Main.java
@@ -16,6 +16,30 @@ class Main {
 3) do-while loop
 
 
+//Syntax  
+==> for loop
+
+for(initialization; condition; update){
+    // code
+}
+
+==> while loop
+initialization
+while(condition){
+    // code
+    increment / decrement
+}
+
+==> do-while loop 
+initialization
+do{
+    // code
+    increment / decrement
+}while(condition);
+
+
+
+
 class Main {
     public static void main(String[] args){
         for(int i=1; i<=5; i++){
@@ -25,6 +49,7 @@ class Main {
 }
 
 
+
 class Main {
     public static void main(String[] args){
         for(int i=1; i<=5; i++){
@@ -34,6 +59,8 @@ class Main {
 }
 
 
+
+
 class Main {
     public static void main(String[] args){
         for(int i=1; i<=5; i=i+2){
@@ -41,6 +68,8 @@ class Main {
         }
     }
 }
+
+
 
 
 class Main {
@@ -59,6 +88,8 @@ class Main {
         }
     }
 }
+
+
 
 
 class Main {
@@ -81,6 +112,8 @@ class Main {
 }
 
 
+
+
 class Main {
     public static void main(String[] args){
         int i=0;
@@ -92,6 +125,7 @@ class Main {
 }
 
 
+
 class Main {
     public static void main(String[] args){
         int i, sum;
@@ -101,20 +135,127 @@ class Main {
         }
     }
 }
+output:
+1
+2
+3
+4
+5
 
 
 
 class Main {
     public static void main(String[] args){
         int i, sum=0;
-        for(i=1; i<=10; i++)
-            {
+        for(i=1; i<=10; i++){
             sum += i;
         }
         System.out.println(sum); //55
     }
 }
 
+
+
+class Main {
+    public static void main(String[] args){
+        for(int i=0, j=0; i<5 && j<3; i++, j++){
+           System.out.println(i); 
+           System.out.println(j); 
+        }
+    }
+}
+output:
+0
+0
+1
+1
+2
+2
+
+
+
+class Main {
+    public static void main(String[] args){
+        for(int i=0, j=5; i<5 && j>0; i++, j--){
+           System.out.println("i="+i+" "+"j="+j); 
+        }
+    }
+}
+output:
+i=0 j=5
+i=1 j=4
+i=2 j=3
+i=3 j=2
+i=4 j=1
+
+
+
+class Main {
+    public static void main(String[] args){
+        boolean cnt = false;
+        for(int i=0;i<10 && !cnt; i++){
+           System.out.print(i); 
+
+        if(i==5){
+            cnt = true;
+        }
+        }
+    }
+}
+output:
+012345
+
+
+
+class Main {
+    public static void main(String[] args){
+        int cnt=0;
+        boolean b = true;
+        while(b){
+            System.out.print(cnt+"  ");
+            cnt++;
+        
+        if(cnt == 4)
+            b=false;
+        }
+    }
+}
+output:
+0  1  2  3
+
+
+
+class Main {
+    public static void main(String[] args){
+        int i=0;
+        do{
+            System.out.println("hello");
+            i++;
+        }
+        while(i<=4);
+    }
+}
+output:
+hello
+hello
+hello
+hello
+hello
+
+
+
+class Main {
+    public static void main(String[] args){
+        int j=4;
+        do{
+            System.out.println(j+" ");
+            j++;
+        }
+        while(j<4);
+    }
+}
+output:
+4
 
 
 
