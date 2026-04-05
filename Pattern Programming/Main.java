af80d55c0745ab3219ea915035b20fbc48ebf600
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-04-05 22:05:08 +0530

    Refactor and expand pattern programming examples

--- Pattern Programming/Main.java
+++ Pattern Programming/Main.java
@@ -308,6 +308,113 @@ output:
 * * * * * 
 
 
+class Main{
+    public static void main(String[] args) {
+        int i,j,k;
+        for(i=0;i<=5;i++){
+            for(j=1;j<=5-i;j++){
+                 System.out.print(j);
+            }
+            for(k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+12345
+1234 
+123  
+12   
+1
+
+
+
+class Main{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            int count = i;
+            for(int k=1;k<=5-i+1;k++){
+                System.out.print(count++);   
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+12345
+2345
+345
+45
+5
+
+
+class Main{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            int count = i;
+            for(int k=1;k<=5-i+1;k++){
+            if(i==1 || k==1 || k==5-i+1){
+                System.out.print(count++);   
+            }
+
+             else{
+                System.out.print(" ");
+            }
+          }
+            System.out.println();
+        }
+    }
+}
+output:
+12345
+2  3
+3 4
+45
+5
+
+
+class Main{
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+* 
+* * * 
+* * * * * 
+* * * * * * * 
+* * * * * * * * * 
+
+
+class Main{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++){
+            for(int k=1;k<=5-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+    *
+   ***
+  *****
+ *******
+*********
+
+
 
 
 
