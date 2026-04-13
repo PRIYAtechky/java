a2444a378f43e83f9bd8fbbbf28ec0cbae4dc578
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-04-13 21:35:09 +0530

    Update Main.java

--- Pattern Programming/Main.java
+++ Pattern Programming/Main.java
@@ -416,6 +416,126 @@ output:
 
 
 
+class Main{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=11-2*i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+ *********
+  *******
+   *****
+    ***
+     *
+
+
+
+class Main{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=11-2*i;j++){
+                if(i==1 || j==1 || j==11-2*i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+ *********
+  *     *
+   *   *
+    * *
+     *
+
+
+class Main{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++){
+            for(int k=1;k<=6-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1;j++){
+                if(i==1 || j==1 || j==2*i-1 || i==5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+     *
+    * *
+   *   *
+  *     *
+ *********
+
+
+
+ class Main{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=5; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+ *****
+  *****
+   *****
+    *****
+     *****
+
+
+class Main{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++){
+            for(int j=1;j<=6-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=5; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+output:
+     *****
+    *****
+   *****
+  *****
+ *****
 
 
 
