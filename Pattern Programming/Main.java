d2fb60fc09cabc6a423763d2f753379233c9288b
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-04-14 22:15:01 +0530

    Refactor pattern programming examples in Main.java

--- Pattern Programming/Main.java
+++ Pattern Programming/Main.java
@@ -649,6 +649,43 @@ output:
 *****
 
 
+class Main{
+    public static void main(String [] arg){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=6-i; j++){
+                System.out.print(" ");
+                }
+            for(int k=1; k<=2*i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+        for(int i=2; i<=5; i++){
+            System.out.print(" ");
+            for(int k=1; k<=9; k++){
+                if(k==1 || k==2 || k==3 || k==7 || k==8 || k==9 || i==5){
+                 System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println( );
+        }
+    }
+}
+output:
+     *
+    ***
+   *****
+  *******
+ *********
+ ***   ***
+ ***   ***
+ ***   ***
+ *********
+
+
 
 
 
