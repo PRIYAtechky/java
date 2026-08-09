ef5f24eed1af100307a31e89cf40db6b84dbaeda
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-08-09 19:34:13 +0530

    Create Main.java

---
+++ File Handling/Main.java
@@ -0,0 +1,87 @@
+/*
+
+File Handling: File handling is used to create, read, write, update, and delete files using Java.
+
+import java.io.File;;
+public class Main{
+    public static void main(String[] args) {
+        File f = new File("D:\\user\\myfiles\\data.txt");
+        System.out.println(f.exists());
+        System.out.println(f.canRead());
+        System.out.println(f.canWrite());
+        System.out.println(f.getName());
+        System.out.println(f.getParent());
+        System.out.println(f.getAbsoluteFile());
+        System.out.println(f.isFile());
+        System.out.println(f.list());
+    }
+}
+output:
+true
+true
+true
+abc.txt
+D:\user\myfiles
+D:\user\myfiles\abc.txt
+true
+null
+
+
+import java.io.File;
+import java.io.IOException;
+public class Main{
+    public static void main(String[] args) {
+        File f = new File("D:\\user\\myfiles\\abc.txt");
+        try{
+            f.createNewFile();  
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }      
+    }
+}
+
+
+
+import java.io.File;
+public class Main{
+    public static void main(String[] args) {
+        File f = new File("D:\\user\\myfiles");
+        String list[]= f.list();
+        for (String l:list){
+            System.out.println(l);
+        }
+             
+    }
+}
+output:
+abc.txt
+data.txt
+
+
+
+import java.io.File;
+public class Main{
+    public static void main(String[] args) {
+        File f = new File("D:\\user\\myfiles\\data.txt");
+        f.delete();
+    }
+}
+    
+
+
+import java.io.File;
+public class Main{
+    public static void main(String[] args) {
+        File f = new File("D:\\user\\myfiles\\data.txt");
+        f.delete();
+    }
+}
+
+
+*/
+
+
+
+
+
