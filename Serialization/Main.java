ac235db7ca1d0960a02563a33955f312b3b8b492
PADMAPRIYA <158708326+PRIYAtechky@users.noreply.github.com>
2026-09-05 23:36:49 +0530

    Create Main.java

---
+++ Serialization/Main.java
@@ -0,0 +1,116 @@
+/*
+
+Serialization – The mechanism of converting an object into a byte stream is called Serialization.
+Deserialization – The mechanism of converting a byte stream back into an object is called Deserialization.
+
+
+=> This program serializes a Customer object into a byte stream and stores it in a file using ObjectOutputStream.
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+class Customer implements Serializable {
+    private String name;
+    private int c_id;
+    private float bal;
+    // Constructor
+    public Customer(String name, int c_id, float bal) {
+        this.name = name;
+        this.c_id = c_id;
+        this.bal = bal;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getC_id() {
+        return c_id;
+    }
+    public void setC_id(int c_id) {
+        this.c_id = c_id;
+    }
+    public float getBal() {
+        return bal;
+    }
+    public void setBal(float bal) {
+        this.bal = bal;
+    }
+}
+public class File {
+    public static void main(String[] args) {
+        Customer c = new Customer("Priya", 123, 100.37f);
+        String path = "D:\\demo\\demo.txt";
+        try {
+            FileOutputStream f = new FileOutputStream(path);
+            ObjectOutputStream o = new ObjectOutputStream(f);
+            o.writeObject(c);
+            o.close();
+            f.close();
+            System.out.println("Object serialized successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+=> This program deserializes a byte stream from a file and converts it back into a Customer object using ObjectInputStream.
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+class Customer implements java.io.Serializable {
+    private String name;
+    private int c_id;
+    private float bal;
+    // Constructor
+    public Customer(String name, int c_id, float bal) {
+        this.name = name;
+        this.c_id = c_id;
+        this.bal = bal;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getC_id() {
+        return c_id;
+    }
+    public void setC_id(int c_id) {
+        this.c_id = c_id;
+    }
+    public float getBal() {
+        return bal;
+    }
+    public void setBal(float bal) {
+        this.bal = bal;
+    }
+}
+public class File {
+    public static void main(String[] args) {
+        String path = "D:\\demo\\demo.txt";
+        try {
+            FileInputStream f = new FileInputStream(path);
+            ObjectInputStream o = new ObjectInputStream(f);
+            Customer customer = (Customer) o.readObject();
+            System.out.println("Customer Name: " + customer.getName());
+            System.out.println("Customer ID: " + customer.getC_id());
+            System.out.println("Customer Balance: " + customer.getBal());
+            o.close();
+            f.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+*/
